0c32e0ce0ee6d44655a73d5c8d5245234d1797b8
clement carvalho <clement@diagram.fr>
2025-04-28 12:08:48 +0200

    entity project

---
+++ src/Entity/Project.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjectRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use DateTimeInterface;
+
+#[ORM\Entity(repositoryClass: ProjectRepository::class)]
+#[Vich\Uploadable]
+class Project
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'string', length: 20)]
+    private ?string $type = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: 'text')]
+    private ?string $description = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $tache = null;
+
+    #[ORM\Column(type: 'text')]
+    private ?string $tacheDescription = null;
+
+    #[ORM\Column(type: 'datetime')]
+    private ?DateTimeInterface $dateDebut = null;
+
+    #[ORM\Column(type: 'datetime')]
+    private ?DateTimeInterface $dateFin = null;
+
+    #[Vich\UploadableField(mapping: 'project_images', fileNameProperty: 'imageName')]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $imageName = null;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private ?DateTimeInterface $updatedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $allowedTypes = ['perso', 'academique', 'professionnel'];
+        if (!in_array($type, $allowedTypes)) {
+            throw new \InvalidArgumentException("Type must be 'perso', 'academique', or 'professionnel'.");
+        }
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getTache(): ?string
+    {
+        return $this->tache;
+    }
+
+    public function setTache(string $tache): self
+    {
+        $this->tache = $tache;
+
+        return $this;
+    }
+
+    public function getTacheDescription(): ?string
+    {
+        return $this->tacheDescription;
+    }
+
+    public function setTacheDescription(string $tacheDescription): self
+    {
+        $this->tacheDescription = $tacheDescription;
+
+        return $this;
+    }
+
+    public function getDateDebut(): ?DateTimeInterface
+    {
+        return $this->dateDebut;
+    }
+
+    public function setDateDebut(DateTimeInterface $dateDebut): self
+    {
+        $this->dateDebut = $dateDebut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?DateTimeInterface
+    {
+        return $this->dateFin;
+    }
+
+    public function setDateFin(DateTimeInterface $dateFin): self
+    {
+        $this->dateFin = $dateFin;
+
+        return $this;
+    }
+
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function getUpdatedAt(): ?DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?DateTimeInterface $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+}
